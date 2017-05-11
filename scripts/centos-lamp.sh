6fab8a84d7118b974107dc2c418b18812bf08f17
Sid <sid@test.com>
2017-05-11 06:57:04 +0530

    updated LAMP scriptwith github files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -30,6 +30,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 #Download started content
-
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/sidmat13/vagrantest/master/files/intex.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/sidmat13/vagrantest/master/files/info.php
 
 service httpd start
